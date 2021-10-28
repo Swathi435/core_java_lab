3563646b00c65eb8dab6b557b87ab6e9e26ac958
Swathi435 <93043376+Swathi435@users.noreply.github.com>
2021-10-28 20:11:47 +0530

    Add files via upload

---
+++ src/core_java_lab1/IncreasingNumber.java
@@ -0,0 +1,48 @@
+package core_java_lab1;
+
+import java.util.Scanner;
+
+public class IncreasingNumber {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		int num = scan.nextInt();
+		
+		if(IncreasingNumber.checkNumber(num)) {
+			System.out.println("Number is  increasing order");
+		}
+		else {
+			System.out.println("Number is not  increasing order");
+		}
+		
+		scan.close();
+	}
+	
+	public static boolean checkNumber(int num) {
+		int num1;
+		int num2;
+		
+		boolean value = false;
+		while(num!=0) {
+			num1 = num%10;
+			num=num/10;
+			num2=num%10;
+			num=num/10;
+			
+			if(num1>=num2) {
+				value = true;
+			}
+			else {
+				value = false;
+				break;
+			}
+		}
+		
+		return value;
+		
+
+	}
+
+}
+			
+			
